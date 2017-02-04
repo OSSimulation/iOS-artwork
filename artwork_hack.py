c471801f74a1c52beff9df5e979d226825be0a00
Jato-BZ <blitzn@vodafone.co.nz>
2017-02-04 22:18:23 +1300

    Create artwork_hack.py
    This script removes the null padding Apple have added to the iOS 10 "@3x" .artwork files which breaks Dave Peck's .artwork extractor tool. I wasn't able to find in Dave's scripts where to apply this script as such so put together this hacky script. This essentially converts the iOS10 @3x .artwork files to iOS9 @3x specs which then extract fine. 
    
    Usage: <artwork_hack.py> <path/to/artwork/file>
    
    After running the script you may need to generate your own JSON's to extract the newly hacked iOS 10 @3x .artwork files.
    I will be providing the JSON's when I can. Should be faster now as I have a series of scripts to do whats required now.

---
+++ artwork_hack.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import struct
+import os
+import os.path
+import sys
+
+def main():
+    
+    cwd = os.getcwd() 
+    artwork_file_path = sys.argv[1] 
+    path, file = os.path.split(artwork_file_path)
+    artwork_file = open(artwork_file_path, 'rb')
+    artwork_file_data = artwork_file.read()
+    artwork_file_data_size = len(artwork_file_data)
+    #
+    # Read end of file footer for total icon count
+    #
+    total_icon_count = struct.unpack('<L', artwork_file_data[(artwork_file_data_size - 8):(artwork_file_data_size - 4)])[0]
+    
+    artwork_file.seek(0)
+    pos = artwork_file.tell()
+    byte_count = 0
+    #
+    # Check to see if the new file to be created already exists
+    #
+    if os.path.isfile(file):
+
+        print "\nFAIL. %s already exists -- don't want to overwrite it." % (file)
+        artwork_file.close()
+        exit()
+
+    else:
+        #
+        # Create new artwork file
+        #
+        new_artwork_file_header = "%s" % (file)
+        new_artwork_file = open(new_artwork_file_header, 'wb')  
+
+
+
+    icon_count = 1
+    row_count = 1
+    total_bytes = 0
+    #
+    # Iterate over the original .artwork file while icon count is less than total icon count
+    # Write png data minus the 32 byte padding
+    # 
+    while icon_count <= total_icon_count:
+
+        if row_count == 88:
+
+            row_count = 1
+            icon_pad = artwork_file_data[pos:pos + 2144]
+            new_artwork_file.write(icon_pad)
+            pos += 3456
+            icon_count += 1
+            total_bytes += 4096
+            #
+            # 87 x 32 = 2784 bytes
+            # total pad = 3456 then minus 2144 = 1312
+            #
+            # Add IF statement to subtract the typical end icon padding for the last icon
+            #
+            if icon_count == total_icon_count:
+                
+                total_bytes += -1312
+            else:
+                
+                pass
+            
+        else:
+    
+            pass
+        #
+        # Write data to new artwork file minus the 32 byte pad Apple have applied
+        #
+        short_bytes = artwork_file_data[pos:pos + 352]
+        new_artwork_file.write(short_bytes)
+        byte_count += 384
+        pos += 384
+        row_count += 1
+  
+    artwork_file.close()
+
+    new_artwork_file.close()
+    new_artwork_file = open(new_artwork_file_header, 'rb')
+    new_artwork_file_data = new_artwork_file.read()
+    total_bytes_new_artwork = len(new_artwork_file_data)
+
+    print "\n%s created using %s as a template" % (new_artwork_file_header, artwork_file_path)
+    print "\nOriginal file size: %d" % artwork_file_data_size
+    print "Total bytes skipped: %d" % total_bytes
+    print "New file size: %d" % total_bytes_new_artwork
+    #print "\nTotal icons: %d" % total_icon_count
+
+if __name__ == "__main__":
+    main()
